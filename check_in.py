b84c457b114ffa3a9de5664eb5c7e204d01f6d1a
ana-tsapina <anastasia.t1223@gmail.com>
2024-09-24 09:22:49 -0400

    Create check_in.py

---
+++ check_in.py
@@ -0,0 +1,16 @@
+#programming check in
+
+birth_year = int(input("Enter your birth year: "))
+current_year = 2024
+
+# long way of doing line 3
+# birth_year = input()
+# birth_year = int(birth_year)
+
+age = current_year - birth_year
+
+if age > 30: 
+    print(f"You are {age} years old")
+else: 
+    print(f"You are {age} years old")
+
