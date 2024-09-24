307bfdc8c8f3a1055db21302a9c4ec64cb469045
ana-tsapina <anastasia.t1223@gmail.com>
2024-09-24 16:57:31 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,29 @@
-("hello world")
+# goal: create a python program that can add, subtract, multiply and divide two numbers
+
+# functional decomposition:
+# obtain a way to get two numbers 
+#obtain a way to add, subtract, multiply, divide
+# do the inputed calculation with the two given numbers 
+#output the calculation/result 
+
+
+print("Welcome to our calculator app!")
+
+#1. grab two numbers
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#2. obtain a way to choose add, subtract, multiply, divide 
+
+choice = input("Choose to either add, subtract, multiply, or divide: ")
+
+if choice == "add": 
+    print(num1 + num2)
+elif choice == "subtract":
+    print (num1 - num2)
+elif choice == "multiply": 
+    print(num1 * num2)
+elif choice == "divide": 
+    print(num1/num2)
+
