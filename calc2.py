2c3d7141dfae9aaeb550185794ad6fa54c4baa2c
ana-tsapina <anastasia.t1223@gmail.com>
2024-09-24 21:07:49 -0400

    Create calc2.py

---
+++ calc2.py
@@ -0,0 +1,68 @@
+#Program Goal: build a Python a second python program that can add, subtract, multiply, and divide, and is better than the first calculator 
+
+#Custom Function 
+
+def numericInput(prompt): 
+
+    while True: 
+
+        value = input(prompt)
+
+
+    try: 
+        value = float(value)
+        return value 
+    except: 
+        print(f"{value} cannot be converted into a floating point. ")
+        
+
+# print("Welcome to our calculator! :)")
+
+def menu(): 
+    menu = """
+
+Calculator Menu Options:
+
+    1. Add 
+    2. Subtract 
+    3. Divide 
+    4. Exit Program 
+
+
+"""
+
+    while True: 
+        print(menu)
+        user_choice = input("User's Choice: ").lower()
+        #string method, edits string to all lower case 
+
+        if user_choice in {'add', 'l', '+'}:
+            return 1
+        elif user_choice in {'subtract', '2', '-'}: 
+            return 2
+        elif user_choice in ('multiply', '3', '*'):
+            return 3
+        elif user_choice in ('divide', '4', '/'):    
+            return 4
+        elif user_choice in ('exit', '5'): 
+            return 5  
+        else: 
+            print(f"{user_choice} is not a menu option")
+
+
+    if menu_result == 1: 
+        print(f"{num1} + {num2} = {answer}")
+    elif menu_result == 2: 
+         print(f"{num1} - {num2} = {answer}")
+    elif menu_result == 3: 
+         print(f"{num1} * {num2} = {answer}")
+    elif menu_result == 4: 
+        try: 
+            answer = num1/num2
+            print(f"{num1} / {num2} = {answer}")
+        except ZeroDivisionError: 
+            print(f"Can't diviide by zero, please try again")
+
+    elif menu_result == 5: 
+         print(f"See you later :) ")
+        break
