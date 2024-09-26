596e54df74be727b7f7150ceaefb10f764af4bb4
ana-tsapina <anastasia.t1223@gmail.com>
2024-09-26 08:57:06 -0400

    Update intro_program.py

--- intro_program.py
+++ intro_program.py
@@ -1,7 +1,19 @@
 # first python program 
 
-first_name = input("Enter your first name:" )
-last_name = input("Enter your last name")
+first_name = input("Enter your first name: " )
+last_name = input("Enter your last name: ")
 
 birth_year = int(input("Enter your birth year: "))
 
+age = 2024 - birth_year
+
+print(first_name, last_name, age) 
+
+message = f"Hello {first_name} {last_name}.\n You are {age} years old"
+
+print(message)
+
+if age >= 19: 
+    print("You are old enough to drink!")
+else: 
+    print("You are not old enough to drink!")
