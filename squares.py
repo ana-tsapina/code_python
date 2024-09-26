ee2df4e5fe1bd1b3c4cb9335b4a64ec1f324a47f
ana-tsapina <anastasia.t1223@gmail.com>
2024-09-26 09:14:12 -0400

    Create squares.py

---
+++ squares.py
@@ -0,0 +1,13 @@
+
+#import statements: 
+
+from math import sqrt, floor 
+
+tiles = int(input())
+
+answer = tiles ** 0.5 
+answer = sqrt(tiles)
+answer = floor(answer)
+
+print(f"The largest square had a side length of {answer}.")
+
