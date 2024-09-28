89604114412f15acbca4dcfcc146b16a6bbdd723
ana-tsapina <anastasia.t1223@gmail.com>
2024-09-28 15:28:44 -0400

    updated

--- triangle_times.py
+++ triangle_times.py
@@ -4,12 +4,16 @@ angle3 = int(input("Enter angle 3: "))
 
 
 
-angle_sum = sum[angle1, angle2, angle3]
+angle_sum = angle1 + angle2 + angle3
 
 
 if angle_sum != 180: 
     print("Error")
 else: 
-    if angle1 == angle2 == angle3 #chainging comparision operator of == 
-
-    wee
+    if angle1 == angle2 == angle3: #chainging comparision operator of == 
+        print("Equilateral")
+    elif angle1 != angle2 and angle2 != angle3 and angle1 != angle3: 
+        print("Scalene")
+    else: 
+        print("Isosceles")
+    
