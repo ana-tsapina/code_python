53faf608196bc70af8a5bbcc7f2665568bc5c7f1
ana-tsapina <anastasia.t1223@gmail.com>
2024-09-28 15:17:33 -0400

    test

---
+++ triangle_times.py
@@ -0,0 +1,15 @@
+angle1 = int(input("Enter angle 1: "))
+angle2 = int(input("Enter angle 2: "))
+angle3 = int(input("Enter angle 3: "))
+
+
+
+angle_sum = sum[angle1, angle2, angle3]
+
+
+if angle_sum != 180: 
+    print("Error")
+else: 
+    if angle1 == angle2 == angle3 #chainging comparision operator of == 
+
+    wee
