87f96558566b37d2aa9453823a048a81399ab011
ana-tsapina <anastasia.t1223@gmail.com>
2024-10-09 20:46:51 -0400

    commit

--- consonant.py
+++ consonant.py
@@ -1,4 +1,4 @@
-def consonant(word):
+def consonant(word, vowel = False):
     '''
     purpose: 
         find consonants in inputted word 
@@ -16,6 +16,17 @@ for character in word:
     word = word.lower()
     if word.isalpha() and (character) not in {'a','e','i','o','u'}:
         ctr += 1
+
+    if not vowel: 
+        
+        return ctr
+    else: 
+            for character in word: 
+                word = word.lower()
+                if word.isalpha() and (character) in {'a','e','i','o','u'}:
+                    ctr += 1
+        
+            return ctr 
 
 print(f"There are {ctr} consonants in your word!")
 
